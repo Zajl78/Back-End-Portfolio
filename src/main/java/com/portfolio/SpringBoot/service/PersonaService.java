612d68ffc033c7d005da3c999252a59a9263bea4
zulyma <zulymajeschke@gmail.com>
2022-05-29 20:05:41 -0300

    Backend integrado

--- src/main/java/com/portfolio/SpringBoot/service/PersonaService.java
+++ src/main/java/com/portfolio/SpringBoot/service/PersonaService.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@Transactional
 public class PersonaService implements IPersonaService {
 
     @Autowired
